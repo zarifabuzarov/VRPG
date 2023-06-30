b7cbf6c7ce5a38da22a22e0c1d9256e4eb35e23e
zarifabuzarov <74652056+zarifabuzarov@users.noreply.github.com>
2023-06-30 21:25:03 +0600

    Update destroy.cs

--- destroy.cs
+++ destroy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr : MonoBehaviour
+public class destroy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
